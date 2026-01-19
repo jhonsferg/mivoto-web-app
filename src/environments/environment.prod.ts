2b4ef692094e731132875ff7f1733e82248cf6eb
jhonsferg <jhon.fernandez.sg@gmail.com>
2026-01-18 23:35:41 -0500

    chore(assets): add assets and environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.mivoto.com/api/v1', // Updated to typical prod URL
+  envName: 'prod',
+};
---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://qa-api.mivoto.com/api/v1',
+  envName: 'qa',
+};
