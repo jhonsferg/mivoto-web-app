d9d457b7e57f090c8424f8dcadfa802e88da78b2
jhonsferg <jhon.fernandez.sg@gmail.com>
2026-01-18 23:32:55 -0500

    config: configure DayJS, JwtModule and fix animations deprecation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,31 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+  importProvidersFrom,
+} from '@angular/core';
+import { JwtModule } from '@auth0/angular-jwt';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeEsPE from '@angular/common/locales/es-PE';
+
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideTranslateService } from '@ngx-translate/core';
+
+import { apiInterceptor } from './core/interceptors/api.interceptor';
+
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import tz from 'dayjs/plugin/timezone';
+import formatter from 'dayjs/plugin/customParseFormat';
+dayjs.extend(tz);
+dayjs.extend(utc);
+dayjs.extend(formatter);
+import 'dayjs/locale/es';
+dayjs.locale('es');
+registerLocaleData(localeEsPE);
 
 import { routes } from './app.routes';
 
@@ -7,6 +33,22 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([apiInterceptor])),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: () => localStorage.getItem('auth_token'),
+        },
+      })
+    ),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'en',
+      lang: 'es'
+    })
+  ],
 };
